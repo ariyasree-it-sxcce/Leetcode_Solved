d1d847336dd4eda57e50d7e608750dc1c65d08b8
aariyasreeskumar123 <ariyasree.it.sxcce@gmail.com>
2025-12-11 22:52:13 +0530

    leetCode Challenge

--- sortedSquares.py
+++ sortedSquares.py
@@ -8,3 +8,26 @@ class Solution(object):
     
 # Time Complexity ---> O(n)+O(n log n) ===> Therefore, O(n log n)
 #Space Complexity ---> O(n)
+
+'''
+ANOTHER APPROACH
+
+def sortedArray(nums):
+    A=[]
+    temp=0
+    for i in nums:
+        x=i*i
+        A.append(x)
+    for i in range(0,len(nums)):
+        for j in range(i+1,len(nums)):
+            if A[i]>A[j]:
+                temp=A[i]
+                A[i]=A[j]
+                A[j]=temp
+    return A
+print(sortedArray([-4,-1,0,3,10]))
+
+
+Time Complexity ---> O(n^2)
+Space Complexity ---> O(n)
+'''
